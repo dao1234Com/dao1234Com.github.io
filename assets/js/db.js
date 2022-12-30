ecd798bda897770198e6a25a386fdd6169a15314
dao1234Com <120771781+dao1234Com@users.noreply.github.com>
2022-12-30 14:28:50 +0800

    Add files via upload

---
+++ assets/js/db.js
@@ -0,0 +1,25 @@
+new Vue({
+    el: "#常用推荐",
+    data: {
+        items: [
+            {
+                img: './images/logos/google.png',
+                title: 'google',
+                Url: 'http://www.google.com',
+                desc: '全球最大的搜索引擎'
+            },
+            {
+                img: './images/logos/baidu.png',
+                title: '百度',
+                Url: 'http://www.baidu.com',
+                desc: '中国占有率最大的搜索引擎'
+            }
+        ]
+    }
+});
+new Vue({
+    el: "#社区资讯",
+    data:{
+        
+    }
+})
